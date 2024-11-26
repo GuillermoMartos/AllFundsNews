fe044e667e58abe5e9d346626809bcfc18a0f921
Guillermo Martos <guille.l.martos@gmail.com>
2024-11-26 10:37:31 +0100

    fix (deploy): add cors --to configurate when frontend deployed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 import { DB_URI } from './helpers/constants';
 import express from 'express';
 import router from './routes/index';
+import cors from 'cors';
 
 const port = process.env.PORT || 3000;
 const app = express();
+//to do: configurate with frontend
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
